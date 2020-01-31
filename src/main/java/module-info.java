321ecc2211caaaf1cb4c6e8979958a5e736ea2cc
Sebastian Hoß <seb@ho.de>
2020-01-31 11:28:55 +0100

    fix #63
    Signed-off-by: Sebastian Hoß <seb@hoß.de>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module wtf.metio.ilo {
 
   requires info.picocli;
 
+  opens wtf.metio.ilo to info.picocli;
   opens wtf.metio.ilo.commands to info.picocli;
   opens wtf.metio.ilo.converter to info.picocli;
   opens wtf.metio.ilo.model to info.picocli;
