ebf68df395c1cd006db5b2dff8c6dc771775cf3f
Sebastian Hoß <sebhoss@pm.me>
2020-08-23 14:27:14 +0200

    tests for ShellRuntimes

---
+++ src/test/java/wtf/metio/ilo/shell/ShellRuntimesTest.java
@@ -0,0 +1,23 @@
+/*
+ * This file is part of ilo. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of ilo,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+package wtf.metio.ilo.shell;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import wtf.metio.ilo.test.ClassTests;
+
+@DisplayName("ShellRuntimes")
+class ShellRuntimesTest {
+
+  @Test
+  @DisplayName("has private constructor")
+  void shouldHavePrivateConstructor() throws NoSuchMethodException {
+    ClassTests.hasPrivateConstructor(ShellRuntimes.class);
+  }
+
+}
