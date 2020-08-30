45a3995dcfb00830c355bd3195876c2b438261aa
Sebastian Hoß <sebhoss@pm.me>
2020-08-30 17:06:07 +0200

    devcontainer options tests

---
+++ src/test/java/wtf/metio/ilo/devcontainer/DevcontainerOptionsTest.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of ilo. It is subject to the license terms in the LICENSE file found in the top-level
+ * directory of this distribution and at http://creativecommons.org/publicdomain/zero/1.0/. No part of ilo,
+ * including this file, may be copied, modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+package wtf.metio.ilo.devcontainer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import wtf.metio.ilo.test.ClassTests;
+
+import java.lang.reflect.Modifier;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("DevcontainerOptions")
+class DevcontainerOptionsTest {
+
+  @Test
+  @DisplayName("has default constructor")
+  void shouldHaveDefaultConstructor() throws NoSuchMethodException {
+    ClassTests.hasDefaultConstructor(DevcontainerOptions.class);
+  }
+
+  @ParameterizedTest
+  @DisplayName("has public fields")
+  @ValueSource(strings = {
+      "shellRuntime",
+      "composeRuntime",
+      "debug",
+      "mountProjectDir",
+      "pull",
+      "removeImage"
+  })
+  void shouldHavePublicProperty(final String field) throws NoSuchFieldException {
+    final var runtime = DevcontainerOptions.class.getDeclaredField(field);
+    assertTrue(Modifier.isPublic(runtime.getModifiers()));
+  }
+
+}
