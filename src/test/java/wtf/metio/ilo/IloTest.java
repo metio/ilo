3d2cf65092881825070c616cb1bdad324283c8b2
Sebastian Hoß <seb@xn--ho-hia.de>
2023-10-28 08:08:33 +0200

    remove failing tests on Java 21
    Signed-off-by: Sebastian Hoß <seb@xn--ho-hia.de>

--- src/test/java/wtf/metio/ilo/IloTest.java
+++ src/test/java/wtf/metio/ilo/IloTest.java
@@ -12,44 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 import uk.org.webcompere.systemstubs.properties.SystemProperties;
-import uk.org.webcompere.systemstubs.security.AbortExecutionException;
-import uk.org.webcompere.systemstubs.security.SystemExit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static wtf.metio.ilo.test.TestResources.testResources;
 
 @DisplayName("Ilo")
 @ExtendWith(SystemStubsExtension.class)
 class IloTest {
-
-  @Test
-  @DisplayName("--help return exit code 0")
-  void exitCodeForHelp(final SystemExit systemExit) {
-    assertThrows(AbortExecutionException.class, () -> Ilo.main("--help"));
-    assertEquals(0, systemExit.getExitCode());
-  }
-
-  @Test
-  @DisplayName("-h return exit code 0")
-  void exitCodeForShortHelp(final SystemExit systemExit) {
-    assertThrows(AbortExecutionException.class, () -> Ilo.main("-h"));
-    assertEquals(0, systemExit.getExitCode());
-  }
-
-  @Test
-  @DisplayName("--version return exit code 0")
-  void exitCodeForVersion(final SystemExit systemExit) {
-    assertThrows(AbortExecutionException.class, () -> Ilo.main("--version"));
-    assertEquals(0, systemExit.getExitCode());
-  }
-
-  @Test
-  @DisplayName("-V return exit code 0")
-  void exitCodeForShortVersion(final SystemExit systemExit) {
-    assertThrows(AbortExecutionException.class, () -> Ilo.main("-V"));
-    assertEquals(0, systemExit.getExitCode());
-  }
 
   @Test
   @DisplayName("reads .rc files")
