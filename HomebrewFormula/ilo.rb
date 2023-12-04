b14f066483f0a824c3f3c9d9b24c47889a0c2b5d
sebhoss <sebhoss@users.noreply.github.com>
2023-12-04 03:54:21 +0000

    Update package-manager infos for latest release

--- HomebrewFormula/ilo.rb
+++ HomebrewFormula/ilo.rb
@@ -5,11 +5,11 @@
 
 class Ilo < Formula
   desc "Manage reproducible build environments"
-  version "2023.11.26"
+  version "2023.12.4"
   homepage "https://ilo.projects.metio.wtf/"
   license "CC0"
-  url "https://github.com/metio/ilo/releases/download/2023.11.26/ilo-2023.11.26-mac.zip"
-  sha256 "7930add064cb828b3100e09d653d753c8a2bc887cff30a29debe644fbf7f55f2"
+  url "https://github.com/metio/ilo/releases/download/2023.12.4/ilo-2023.12.4-mac.zip"
+  sha256 "fff4e378271c6239934575e4fe9ce74128303fb2d6dabee0b7d230997b9b6ac4"
 
   depends_on "podman" => :optional
   depends_on "docker" => :optional
